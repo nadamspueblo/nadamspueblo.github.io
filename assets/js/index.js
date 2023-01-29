413d785943c7d052727e46fc5cd79dcaeab3a45b
nadamspueblo <47441281+nadamspueblo@users.noreply.github.com>
2023-01-29 16:09:03 -0700

    Testing a method to scroll screen to hide browser bar on mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,7 @@ loadNewsItem();
 
 function startTypingTitle() {
   location.hash = "#top";
+  window.scrollTop = 0;
   window.scrollTo(0, 1);
   var anim = new TypingAnimation(document.getElementById("site-title"), document.getElementById("site-title-text").innerHTML, 100);
   anim.start()
