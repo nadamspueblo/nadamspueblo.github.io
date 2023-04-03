b6940af4de3986576104ffe11d5db8831ec4db42
nadamspueblo <47441281+nadamspueblo@users.noreply.github.com>
2023-04-02 22:16:18 -0700

    Added JS Lint, still some bugs

--- assets/js/base-scripts.js
+++ assets/js/base-scripts.js
@@ -46,6 +46,162 @@ for (var i = 0; i < navLinks.length; i++) {
 }
 
 
+/** Preview-expand panel functionality */
+const expandTabs = document.getElementsByClassName("preview-expand-tab");
+const expandPanels = document.getElementsByClassName("preview-expand");
 
+for (let i = 0; i < expandTabs.length; i++) {
+  expandPanels[i].style.maxHeight = "200px";
+  expandTabs[i].addEventListener("click", function () {
+    /* Toggle between hiding and showing the active panel */
+    var tab = expandTabs[i];
+    var panel = expandPanels[i];
+    console.log(panel.style.maxHeight);
+    if (panel.style.maxHeight === "200px") {
+      tab.innerHTML = "Less [-]";
+      panel.style.maxHeight = "fit-content";
+
+    } else {
+      tab.innerHTML = "More [+]";
+      panel.style.maxHeight = "200px";
+    }
+  });
+}
+
+/** Linting of code elements */
+const jsKeyWords = ["var", "let", "const", "function"];
+const jsBrackets = ["[", "]", "{", "}", "(", ")"];
+const jsControl = ["if ", "for ", "while "];
+const jsOperators = [" < ", " > ", " <= ", " >= ", "++", " - ", " + ", "--", " / ", " % ", " * ", ";"];
+const codeElements = document.getElementsByClassName("jscode");
+//const isNumeric = n => !isNaN(n);
+const isNumeric = n => /\d|\./.test(n);
+console.log(lint("for (let i = 0; i < 10; i++){"));
+
+for (let e of codeElements) {
+  e.innerHTML = lint(e.innerHTML);
+}
+
+function lint(code) {
+  // Remove html escape chars
+  code = code.replaceAll("&lt;", "<");
+  code = code.replaceAll("&gt;", ">");
+
+  // Start lint process with block comments
+  code = lintComments(code);
+
+  // Replace html escape chars
+  code = code.replaceAll(" < ", " &lt; ");
+  code = code.replaceAll(" > ", " &gt; ");
+  code = code.replaceAll(" <= ", " &lt;= ");
+  code = code.replaceAll(" >= ", " &gt;= ");
+  return code;
+}
+
+function lintComments(code) {
+  let result = "";
+  let startIndex = code.indexOf("/*");
+  let endIndex = 0;
+  while (startIndex >= 0 && endIndex < code.length) {
+    result += lintInlineComments(code.substring(endIndex, startIndex));
+    result += "<span class='jscomment'>";
+    endIndex = code.indexOf("*/", startIndex) + 3;
+    result += code.substring(startIndex, endIndex) + "</span>";
+    startIndex = code.indexOf("/*", endIndex);
+  }
+  if (code.lastIndexOf("*/") >= 0) {
+    startIndex = code.lastIndexOf("*/") + 3;
+  }
+  else {
+    startIndex = 0;
+  }
+  result += lintInlineComments(code.substring(startIndex));
+  return result;
+}
+
+function lintInlineComments(code) {
+  let result = "";
+  let startIndex = code.indexOf("//");
+  let endIndex = 0;
+  while (startIndex >= 0 && endIndex < code.length) {
+    result += lintStrings(code.substring(endIndex, startIndex));
+    result += "<span class='jscomment'>";
+    endIndex = code.indexOf("\n", startIndex) + 1;
+    result += code.substring(startIndex, endIndex) + "</span>";
+    startIndex = code.indexOf("//", endIndex);
+  }
+  if (code.lastIndexOf("//") >= 0) {
+    startIndex = code.lastIndexOf("//") + 1;
+    startIndex = code.indexOf("\n", startIndex);
+  }
+  else {
+    startIndex = 0;
+  }
+  result += lintStrings(code.substring(startIndex));
+  return result;
+}
+
+function lintStrings(code) {
+  let result = "";
+  let startIndex = code.indexOf("\"");
+  let endIndex = 0;
+  while (startIndex >= 0 && endIndex < code.length) {
+    result += lintNumbers(code.substring(endIndex, startIndex));
+    result += "<span class='jsstring'>";
+    endIndex = code.indexOf("\"", startIndex + 1) + 1;
+    result += code.substring(startIndex, endIndex) + "</span>";
+    startIndex = code.indexOf("\"", endIndex);
+  }
+  if (code.lastIndexOf("\"") >= 0) {
+    startIndex = code.lastIndexOf("\"") + 1;
+  }
+  else {
+    startIndex = 0;
+  }
+  result += lintNumbers(code.substring(startIndex));
+  return result;
+}
+
+console.log(" i < 10".search(/\d/))
+function lintNumbers(code) {
+  let result = "";
+  let startIndex = code.search(/\d/);
+  let endIndex = 0;
+  while (startIndex >= 0 && startIndex >= endIndex && endIndex < code.length) {
+    result += lintKeywords(code.substring(endIndex, startIndex));
+    endIndex = startIndex;
+    while (endIndex < code.length && isNumeric(code.charAt(endIndex))) {
+      endIndex++;
+    }
+    if (endIndex >= startIndex){
+      result += "<span class='jsnumber'>";
+      result += code.substring(startIndex, endIndex) + "</span>";
+      startIndex += code.substring(endIndex).search(/\d/);
+    }
+    else {
+      break;
+    }
+  }
+  result += lintKeywords(code.substring(endIndex));
+  return result;
+}
+
+function lintKeywords(code) {
+  // = must come first because of = in html attribs
+  code = code.replaceAll("=", "<span class='jsoperator'>=</span>");
+  for (let k of jsOperators) {
+    code = code.replaceAll(k, "<span class='jsoperator'>" + k + "</span>");
+  }
+  for (let k of jsKeyWords) {
+    code = code.replaceAll(k, "<span class='jskeyword'>" + k + "</span>");
+  }
+  for (let k of jsBrackets) {
+    code = code.replaceAll(k, "<span class='jsbracket'>" + k + "</span>");
+  }
+  for (let k of jsControl) {
+    code = code.replaceAll(k, "<span class='jscontrol'>" + k + "</span>");
+  }
+  return code;
+}
 
 console.log("Script loaded");
